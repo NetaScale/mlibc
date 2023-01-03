34f723ae1404f27f2e140da9af5d7544b2055fcb
NetBSD User <netbsduser@users.noreply.github.com>
2023-01-03 14:30:49 +0000

    Minor alterations for SCAL/UX

--- options/linux/include/sys/sysmacros.h
+++ options/linux/include/sys/sysmacros.h
@@ -7,17 +7,17 @@ extern "C" {
 
 #include <bits/inline-definition.h>
 
-__MLIBC_INLINE_DEFINITION unsigned int __mlibc_dev_major(
+static inline unsigned int __mlibc_dev_major(
 		unsigned long long int __dev) {
   return ((__dev >> 8) & 0xfff) | ((unsigned int)(__dev >> 32) & ~0xfff);
 }
 
-__MLIBC_INLINE_DEFINITION unsigned int __mlibc_dev_minor(
+static inline unsigned int __mlibc_dev_minor(
 		unsigned long long int __dev) {
   return (__dev & 0xff) | ((unsigned int)(__dev >> 12) & ~0xff);
 }
 
-__MLIBC_INLINE_DEFINITION unsigned long long int __mlibc_dev_makedev(
+static inline unsigned long long int __mlibc_dev_makedev(
 		unsigned int __major, unsigned int __minor) {
   return ((__minor & 0xff) | ((__major & 0xfff) << 8)
 	  | (((unsigned long long int)(__minor & ~0xff)) << 12)
